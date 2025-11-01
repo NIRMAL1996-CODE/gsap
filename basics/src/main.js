93958e1db31ba23b4c0bd05ad43283a70352e24b
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-11-01 16:01:37 +0530

    GSAP animation

--- basics/src/main.js
+++ basics/src/main.js
@@ -1,22 +1,26 @@
 import { gsap } from "gsap";
-gsap.to("#box1", {
+gsap.to(".box1", {
   x:1160,
-  duration:3,
+  duration:5,
   delay:1,
   rotate:360,
   backgroundColor:"pink",
-  borderRadius:"50%",
+  borderRadius:"100%",
   repeat:-1,
+  stagger: 0.5,
   yoyo:true 
 })
 
-gsap.from("#box2", {
+gsap.from(".box2", {
   x:1160,
-  duration:3,
+  duration:5,
   delay:1,
   rotate:360,
   backgroundColor:"pink",
-  borderRadius:"50%",
+  borderRadius:"100%",
   repeat:-1,
+  stagger: 0.5,
   yoyo:true 
 })
+
+

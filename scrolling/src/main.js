f8e999a4180837d20b15f65be9d16506679d788b
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-11-02 19:33:22 +0530

    Scrolling Marquee

---
+++ scrolling/src/main.js
@@ -0,0 +1,32 @@
+import "./style.css";
+import gsap  from "gsap";
+
+
+function wheelAnimation(){
+  window.addEventListener("wheel",function(dets){
+  if(dets.deltaY>0){
+   gsap.to(".marque",{
+    transform:"translateX(-200%)",
+    repeat:-1,
+    duration:4,
+    ease:"none"
+   })
+   gsap.to(".marque img",{
+    rotate:180,
+   })
+  }
+  else{
+     gsap.to(".marque",{
+    transform:"translateX(0%)",
+    repeat:-1,
+    duration:4,
+    ease:"none"
+   })
+   gsap.to(".marque img",{
+    rotate:0,
+   })
+  }
+});
+}
+
+wheelAnimation();

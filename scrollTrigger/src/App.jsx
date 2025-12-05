13288a0eb48aac325103927af3beaa723877d332
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-12-05 22:26:56 +0530

    setting markers

--- scrollTrigger/src/App.jsx
+++ scrollTrigger/src/App.jsx
@@ -12,14 +12,14 @@ const App = () => {
 	const container = useRef();
   
    useGSAP(() => {
-    gsap.from(container.current, {   
-      //  x: 150,            // (meaning: start from down)
-      opacity: 0,        // (meaning: start invisible)
-      duration: 1, 
+    gsap.from(container.current, { 
+     rotate:90,
+      duration: 1,
+      delay:1, 
       scrollTrigger: {
         trigger: container.current,
-        start: "top 30%",   // (meaning: when box top reaches 80% of screen)
-        end: "top -70%",
+        start: "top 50%",   // (meaning: when box top reaches 80% of screen)
+        end: "top 0%",
         scrub: true,
         markers:true,          // (meaning: animation moves with scroll)
       }
@@ -28,16 +28,16 @@ const App = () => {
 
   return (
     <div >
-    <div  className='h-screen flex justify-center items-center'>
+    <div  className='bg-gray-500 h-screen flex justify-center items-center '>
       <h1 ref={container} className='text-9xl'>hello1</h1>
      </div>
-     <div className='h-screen flex justify-center items-center'>
+     <div className='bg-gray-400 h-screen flex justify-center items-center'>
       <h1 className='text-9xl'>hello2</h1>
      </div>
-     <div className='h-screen flex justify-center items-center'>
+     <div className='bg-gray-300 h-screen flex justify-center items-center'>
       <h1 className='text-9xl'>hello3</h1>
      </div>
-     <div className='h-screen flex justify-center items-center'>
+     <div className=' bg-gray-200 h-screen flex justify-center items-center'>
       <h1 className='text-9xl'>hello4</h1>
      </div>
     </div>

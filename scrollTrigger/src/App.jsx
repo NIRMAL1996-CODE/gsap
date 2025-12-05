7ab83c0f5f7b4f7eeb13de5fb59223be39506b2b
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-12-05 23:07:47 +0530

    Animation with images

--- scrollTrigger/src/App.jsx
+++ scrollTrigger/src/App.jsx
@@ -9,36 +9,99 @@ gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
 
-	const container = useRef();
+	// const container = useRef();
+const box1 = useRef();
+const box2 = useRef();
+const box3 = useRef();
+const box4 = useRef();
+
   
-   useGSAP(() => {
-    gsap.from(container.current, { 
-     rotate:90,
-      duration: 1,
-      delay:1, 
-      scrollTrigger: {
-        trigger: container.current,
-        start: "top 50%",   // (meaning: when box top reaches 80% of screen)
-        end: "top 0%",
-        scrub: true,
-        markers:true,          // (meaning: animation moves with scroll)
-      }
-    });
-  }, { scope:container});
+  //  useGSAP(() => {
+  //   gsap.from(container.current, { 
+  //    rotate:90,
+  //     duration: 1,
+  //     delay:1, 
+  //     scrollTrigger: {
+  //       trigger: container.current,
+  //       start: "top 50%",   // (meaning: when box top reaches 80% of screen)
+  //       end: "top 0%",
+  //       scrub: true,
+  //       markers:true,          // (meaning: animation moves with scroll)
+  //     }
+  //   });
+  // }, { scope:container});
+
+  useGSAP(() => {
+  gsap.from(box1.current, {
+    rotate: 360,
+    scrollTrigger: {
+      trigger: box1.current,
+      start: "top 60%",
+      end: "top 0%",
+      scrub: true,
+      markers:true,
+    }
+  });
+});
+
+useGSAP(() => {
+  gsap.from(box2.current, {
+    scale: 0,
+    scrollTrigger: {
+      trigger: box2.current,
+      start: "top 60%",
+      end: "top 0%",
+      scrub: true,
+      markers:true,
+    }
+  });
+});
+
+useGSAP(() => {
+  gsap.to(box3.current, {
+    x: -400,
+    scrollTrigger: {
+      trigger: box3.current,
+      start: "top 30%",
+      end: "top 0%",
+      scrub: true,
+      markers:true,
+    }
+  });
+});
+
+useGSAP(() => {
+  gsap.from(box4.current, {
+    x: 900,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: box4.current,
+      start: "top 60%",
+      end: "top 30%",
+      scrub: true,
+      markers:true,
+    }
+  });
+});
+ 
 
   return (
     <div >
-    <div  className='bg-gray-500 h-screen flex justify-center items-center '>
-      <h1 ref={container} className='text-9xl'>hello1</h1>
+    <div  className=' h-screen flex flex-col justify-center items-center '>
+      <img className="h-[400px]" ref={box1} src="/img1.png" alt="fries" />
+      <h1 className='text-6xl'>French Fries</h1>
      </div>
-     <div className='bg-gray-400 h-screen flex justify-center items-center'>
-      <h1 className='text-9xl'>hello2</h1>
+     <div className=' h-screen flex flex-col justify-center items-center'>
+      <img ref={box2} className="h-[400px]" src="/img2.png" alt="" />
+      <h1 className='text-6xl'>Snacks</h1>
      </div>
-     <div className='bg-gray-300 h-screen flex justify-center items-center'>
-      <h1 className='text-9xl'>hello3</h1>
+     <div className=' h-screen flex flex-col justify-center items-center'>
+      <img ref={box3} className="h-[600px]" src="/img3.png" alt="" />
+      <h1 className='text-6xl'>Burger</h1>
      </div>
-     <div className=' bg-gray-200 h-screen flex justify-center items-center'>
-      <h1 className='text-9xl'>hello4</h1>
+     <div className='  h-screen flex flex-col justify-center items-center'>
+      <img ref={box4} className="h-[600px]" src="/img4.png" alt="" />
+      <h1 className='text-6xl'>chips</h1>
      </div>
     </div>
   )
